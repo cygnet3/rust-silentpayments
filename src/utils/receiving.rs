5c606ee9dd8d4d60d9a00ad0191df66078f19c14
Sosthene00 <674694@protonmail.ch>
2024-02-07 15:55:23 +0100

    Bug fix: handle empty input_pubkeys

--- src/utils/receiving.rs
+++ src/utils/receiving.rs
@@ -2,11 +2,11 @@ use crate::{utils::hash_outpoints, Result};
 use secp256k1::{PublicKey, SecretKey};
 
 pub fn recipient_calculate_tweak_data(
-    input_pub_keys: &Vec<PublicKey>,
-    outpoints: &Vec<(String, u32)>,
+    input_pub_keys: &[&PublicKey],
+    outpoints: &[(String, u32)],
 ) -> Result<PublicKey> {
     let secp = secp256k1::Secp256k1::new();
-    let A_sum = recipient_get_A_sum_public_keys(input_pub_keys);
+    let A_sum = PublicKey::combine_keys(input_pub_keys)?;
     let outpoints_hash = hash_outpoints(outpoints, A_sum)?;
 
     Ok(A_sum.mul_tweak(&secp, &outpoints_hash)?)
@@ -19,10 +19,4 @@ pub fn recipient_calculate_shared_secret(
     let secp = secp256k1::Secp256k1::new();
 
     Ok(tweak_data.mul_tweak(&secp, &b_scan.into())?)
-}
-
-fn recipient_get_A_sum_public_keys(input: &Vec<PublicKey>) -> PublicKey {
-    let keys_refs: &Vec<&PublicKey> = &input.iter().collect();
-
-    PublicKey::combine_keys(keys_refs).unwrap()
 }
