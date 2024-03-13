5af1c7c1bcaae35f20fb4fed71a27bde4385f7ae
cygnet <cygnet3@proton.me>
2024-03-13 17:41:02 +0100

    Give tagged hashed pub(crate) visibility

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use crate::{receiving::Label, Error};
+use crate::Error;
 use bitcoin_hashes::{hash160, sha256t_hash_newtype, Hash, HashEngine};
 use secp256k1::{Parity::Even, PublicKey, Scalar, SecretKey, XOnlyPublicKey};
 
@@ -179,62 +179,63 @@ pub fn get_pubkey_from_input(
 }
 
 sha256t_hash_newtype! {
-    pub struct InputsTag = hash_str("BIP0352/Inputs");
+    pub(crate) struct InputsTag = hash_str("BIP0352/Inputs");
 
     /// BIP0352-tagged hash with tag \"Inputs\".
     ///
     /// This is used for computing the inputs hash.
     #[hash_newtype(forward)]
-    pub struct InputsHash(_);
+    pub(crate) struct InputsHash(_);
 
-    pub struct LabelTag = hash_str("BIP0352/Label");
+    pub(crate) struct LabelTag = hash_str("BIP0352/Label");
 
     /// BIP0352-tagged hash with tag \"Label\".
     ///
     /// This is used for computing the label tweak.
     #[hash_newtype(forward)]
-    pub struct LabelHash(_);
+    pub(crate) struct LabelHash(_);
 
-    pub struct SharedSecretTag = hash_str("BIP0352/SharedSecret");
+    pub(crate) struct SharedSecretTag = hash_str("BIP0352/SharedSecret");
 
     /// BIP0352-tagged hash with tag \"SharedSecret\".
     ///
     /// This hash type is for computing the shared secret.
     #[hash_newtype(forward)]
-    pub struct SharedSecretHash(_);
+    pub(crate) struct SharedSecretHash(_);
 }
 
 impl InputsHash {
-    pub fn from_outpoint_and_A_sum(smallest_outpoint: &[u8; 36], A_sum: &PublicKey) -> InputsHash {
+    pub(crate) fn from_outpoint_and_A_sum(
+        smallest_outpoint: &[u8; 36],
+        A_sum: PublicKey,
+    ) -> InputsHash {
         let mut eng = InputsHash::engine();
         eng.input(smallest_outpoint);
         eng.input(&A_sum.serialize());
         InputsHash::from_engine(eng)
     }
-    pub fn to_scalar(self) -> Scalar {
+    pub(crate) fn to_scalar(self) -> Scalar {
         // This is statistically extremely unlikely to panic.
         Scalar::from_be_bytes(self.to_byte_array()).expect("hash value greater than curve order")
     }
 }
 
 impl LabelHash {
-    pub fn from_b_scan_and_m(b_scan: SecretKey, m: u32) -> LabelHash {
+    pub(crate) fn from_b_scan_and_m(b_scan: SecretKey, m: u32) -> LabelHash {
         let mut eng = LabelHash::engine();
         eng.input(&b_scan.secret_bytes());
         eng.input(&m.to_be_bytes());
         LabelHash::from_engine(eng)
     }
 
-    pub fn to_label(self) -> Label {
+    pub(crate) fn to_scalar(self) -> Scalar {
         // This is statistically extremely unlikely to panic.
-        let s = Scalar::from_be_bytes(self.to_byte_array())
-            .expect("hash value greater than curve order");
-        s.into()
+        Scalar::from_be_bytes(self.to_byte_array()).expect("hash value greater than curve order")
     }
 }
 
 impl SharedSecretHash {
-    pub fn from_ecdh_and_k(ecdh: &PublicKey, k: u32) -> SharedSecretHash {
+    pub(crate) fn from_ecdh_and_k(ecdh: &PublicKey, k: u32) -> SharedSecretHash {
         let mut eng = SharedSecretHash::engine();
         eng.input(&ecdh.serialize());
         eng.input(&k.to_be_bytes());
